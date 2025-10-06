7ecccad2c0e25122f0793e86b20389404da95c8a
EricaDiffo <diffoerica@icloud.com>
2025-10-06 16:03:03 +0200

    Les fonctionnalites du concert

--- src/Festy/presentation/urls.py
+++ src/Festy/presentation/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from Festy.presentation.views.view_artist import artist_list, artist_create
+from Festy.presentation.views.view_concert import concert_list, concert_create, concert_update, concert_delete
 
 urlpatterns = [
     path('list_artist/',artist_list, name='list_artist'),
-    path('create_artist/',artist_create, name='create_artist'), 
+    path('create_artist/',artist_create, name='create_artist'),
+    path('list_concert/',concert_list, name='list_concert'),
+    path('create_concert/',concert_create, name='create_concert'),
+    path('update_concert/',concert_update, name='update_concert'),
+    path('delete_concert/',concert_delete, name='delete_concert'), 
+
 ]
