fa6f785363b4a8a23e394b79a9783a7e28391062
karlessaka <essakakarl01@gmail.com>
2025-10-06 14:36:14 +0200

    fonctionalités_artiste

--- src/Festy/presentation/urls.py
+++ src/Festy/presentation/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from Festy.presentation.views.view_artist import artist_list, artist_create
+from Festy.presentation.views.view_artist import artist_list, artist_create, artist_detail, artist_update, artist_delete
 
 urlpatterns = [
     path('list_artist/',artist_list, name='list_artist'),
     path('create_artist/',artist_create, name='create_artist'), 
+    path('detail_artist/<int:artist_id>/', artist_detail, name='detail_artist'),
+    path('update_artist/<int:artist_id>/', artist_update, name='update_artist'),
+    path('artist_delete/<int:artist_id>/', artist_delete, name='delete_artist'), 
 ]
