171fdc797e09df4120c0020e2d8a0372cadbd1aa
Peughouia <peughouia.44@gmail.com>
2025-10-07 04:30:18 +0200

    gestion de user

--- src/Festy/metier/model/models.py
+++ src/Festy/metier/model/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+class Administrator(AbstractUser):
+    phone = models.CharField(max_length=15, unique=True)
 
+    def __str__(self):
+        return self.username
 
 class Artist(models.Model):
     style_choices = [
@@ -42,13 +47,7 @@ class Booking(models.Model):
         return f"Booking for {self.user_name} - {self.concert.title} at {self.concert.location} enjoy!"
     
 
-class Administrator(models.Model):
-    username = models.CharField(max_length=30, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=50)  
 
-    def __str__(self):
-        return self.username
 
 
     
--- src/Show/settings.py
+++ src/Show/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework',
     'Festy',
 ]
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "Festy.Administrator"
+
